10a85ce34e4df774927405b6ccc8e7bbecbd578e
Brian Chou <brianchou_5@hotmail.com>
2021-04-09 16:34:26 +0800

    Updated DB information

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  HOST: "localhost",
-  USER: "testuser",
-  PASSWORD: "1qaz2wsx",
-  DB: "testdb",
+  HOST: "us-cdbr-east-03.cleardb.com",
+  USER: "b4f9483da30139",
+  PASSWORD: "9b2069de",
+  DB: "heroku_54a01c93cd31086",
   dialect: "mysql",
   pool: {
     max: 5,
